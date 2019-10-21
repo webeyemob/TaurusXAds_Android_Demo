2f31a5819bf676740c4c5cf470a3b048ec66145e
songshun.zhang <shun@macdeiMac.lan>
2019-10-22 00:27:04 +0800

    update some adunits

--- app/src/main/java/com/taurusx/ads/demo/constance/Constance.java
+++ app/src/main/java/com/taurusx/ads/demo/constance/Constance.java
@@ -7,11 +7,13 @@ public class Constance {
     public static final String BUNDLE_TYPE_NATIVE = "native";
     public static final String BUNDLE_TYPE_INTERSTITIAL = "interstitial";
     public static final String BUNDLE_TYPE_REWARDED = "rewarded";
+    public static final String BUNDLE_TYPE_FEEDLIST = "feedlist";
 
-    public static final String CREATIVE_BANNER_ID = "5502dfa5-6652-42fb-9e7e-215337799789";
-    public static final String CREATIVE_NATIVE_ID = "5a28316a-0798-44d1-817e-119998c53643";
-    public static final String CREATIVE_INTERSTITIAL_ID = "5568ac9e-be1e-48d6-90f4-726d2f71bc94";
-    public static final String CREATIVE_REWARDED_ID = "9c9509b3-b4cf-4354-a515-1e754657ef6e";
+    public static final String CREATIVE_BANNER_ID = "324fd5e3-fa2e-4f09-b13a-8a855696f9ea";
+    public static final String CREATIVE_NATIVE_ID = "4e43b149-eade-4d37-a068-c1995bac39c7";
+    public static final String CREATIVE_INTERSTITIAL_ID = "6689e8ad-02af-4c36-a273-a420a1d15624";
+    public static final String CREATIVE_REWARDED_ID = "b5bdab98-b841-459f-ba0b-ad5c8ee60ce2";
+    public static final String CREATIVE_FEEDLIST_ID = "ebfabdbf-9308-4d99-9aca-a0b39fe0ef3d";
 
     public static final String SERIAL_BANNER_ID = "1ccb6f6d-8f16-426f-95e8-12726a459bb7";
     public static final String SERIAL_NATIVE_ID = "8b6cec11-2192-41e6-a99a-2c1c5dac65cc";
