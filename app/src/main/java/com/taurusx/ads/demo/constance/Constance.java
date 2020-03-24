d4a9f33c5438884ca7d9061438390c61fdd8ab34
sszhang <zhangsongshun@163.com>
2020-03-24 13:43:28 +0800

    add page adapter

--- app/src/main/java/com/taurusx/ads/demo/constance/Constance.java
+++ app/src/main/java/com/taurusx/ads/demo/constance/Constance.java
@@ -4,9 +4,7 @@ public class Constance {
     public static final String APP_ID = "4b4b6832-4267-42db-9c04-d517d5288bbb";
 
     public static final String BUNDLE_NETWORK_NAME = "name";
-
     public static final String BUNDLE_TYPE_MEDIATION_NAME = "adName";
-
 
     public static final String BUNDLE_TYPE_BANNER = "banner";
     public static final String BUNDLE_TYPE_NATIVE = "native";
@@ -20,29 +18,5 @@ public class Constance {
 
     public static final String BUNDLE_TYPE_JSON = "type";
     public static final String BUNDLE_TYPE_INFO = "info";
-
-//    public static final String CREATIVE_BANNER_ID = "324fd5e3-fa2e-4f09-b13a-8a855696f9ea";
-//    public static final String CREATIVE_NATIVE_ID = "4e43b149-eade-4d37-a068-c1995bac39c7";
-//    public static final String CREATIVE_INTERSTITIAL_ID = "6689e8ad-02af-4c36-a273-a420a1d15624";
-//    public static final String CREATIVE_REWARDED_ID = "b5bdab98-b841-459f-ba0b-ad5c8ee60ce2";
-//    public static final String CREATIVE_FEEDLIST_ID = "ebfabdbf-9308-4d99-9aca-a0b39fe0ef3d";
-//    public static final String CREATIVE_SPLASH_ID = "43386bc9-8a52-4896-a810-6189b19c36f7";
-//    public static final String CREATIVE_MIXVIEW_ID = "dfaf9366-b8ca-42b4-ad4f-f20d1067b3f7";
-//    public static final String CREATIVE_MIXFULLSCREEN_ID = "14b4a632-4e9f-46ca-a5ee-fd67222c3fcb";
-//
-//    public static final String SERIAL_BANNER_ID = "19364f08-f5a9-4cd5-90ad-7ca8a7eb5f96";
-//    public static final String SERIAL_NATIVE_ID = "4370b131-ce02-47ac-b127-c200207e1473";
-//    public static final String SERIAL_INTERSTITIAL_ID = "43caccf2-e543-41d6-9eac-15a00b1e987d";
-//    public static final String SERIAL_REWARDED_ID = "8ee65a58-5d40-49e5-b0e2-74cb3a8f8994";
-//
-//    public static final String PARALLEL_BANNER_ID = "27f949d5-b898-4857-a861-5a50a386e0c9";
-//    public static final String PARALLEL_NATIVE_ID = "73a300ed-6d15-448a-ae54-3d06dd182d6a";
-//    public static final String PARALLEL_INTERSTITIAL_ID = "aead71f0-245a-4e60-ac55-f12b1bdee297";
-//    public static final String PARALLEL_REWARDED_ID = "5e216843-2352-4d26-a87f-5b23b0cb30b3";
-//
-//    public static final String SHUFFLE_BANNER_ID = "85a50861-d5bd-4306-9a66-d4fb919389a3";
-//    public static final String SHUFFLE_NATIVE_ID = "9b0c1021-3664-49ce-82a8-7a9ec0cd15e8";
-//    public static final String SHUFFLE_INTERSTITIAL_ID = "25b370ef-df02-4dd3-bdd8-fe198e82b89d";
-//    public static final String SHUFFLE_REWARDED_ID = "f8515dd7-5e61-4d9b-abc2-7d9e0dd198ab";
 
 }
