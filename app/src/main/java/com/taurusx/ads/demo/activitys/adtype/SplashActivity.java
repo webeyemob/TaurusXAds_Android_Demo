e0d76bcab74abfc1dec56ac0b98a95a97d361b4d
hang.wang <hang.wang@webeye.com>
2020-11-17 16:48:22 +0800

    KuaiShou: 3.3.5

--- app/src/main/java/com/taurusx/ads/demo/activitys/adtype/SplashActivity.java
+++ app/src/main/java/com/taurusx/ads/demo/activitys/adtype/SplashActivity.java
@@ -1,10 +1,9 @@
 package com.taurusx.ads.demo.activitys.adtype;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.FrameLayout;
-
-import androidx.fragment.app.FragmentActivity;
 
 import com.taurusx.ads.core.api.ad.SplashAd;
 import com.taurusx.ads.core.api.ad.networkconfig.NetworkConfigs;
@@ -19,7 +18,7 @@ import com.taurusx.ads.mediation.networkconfig.SigmobSplashConfig;
 import com.taurusx.ads.mediation.networkconfig.TikTokAppDownloadListener;
 import com.taurusx.ads.mediation.networkconfig.TikTokSplashConfig;
 
-public class SplashActivity extends FragmentActivity {
+public class SplashActivity extends Activity {
 
     private final String TAG = "SplashActivity";
 
